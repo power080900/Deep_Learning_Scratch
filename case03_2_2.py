d61d895bf87dcbeefc702d834fdda8f6d1edbb73
LEE <power080900@gmail.com>
2022-12-23 11:57:54 +0900

    Deep_Learning_Study_221223

--- case03_2_2.py
+++ case03_2_2.py
@@ -1,17 +1,13 @@
 import numpy as np
 import matplotlib.pylab as plt
-
-def step_function(x):
-    return np.array(x > 0, dtype=np.int)
+from lib.util import sigmoid
+from lib.util import step_function
 
 x = np.arange(-5.0, 5.0, 0.1)
 y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
 
 x2 = np.array([-1.0, 1.0, 2.0])
 print(sigmoid(x2))
@@ -23,5 +19,4 @@ plt.plot(a,b)
 plt.ylim(-0.1, 1.1)
 plt.show()
 
-def relu(n):
-    return np.maximum(0, n)
+
