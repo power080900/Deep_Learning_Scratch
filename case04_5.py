097647d0fda97388c1ffdad8bc2d4a8f0d6a816b
power080900 <power080900@gmail.com>
2023-01-08 13:21:03 +0900

    Deep_Learning_Study_230108

--- case04_5.py
+++ case04_5.py
@@ -1,6 +1,6 @@
-
 import sys, os
 sys.path.append(os.pardir)
+import numpy as np
 from dataset.util import *
 
 class TwoLauyerNet:
@@ -12,5 +12,37 @@ class TwoLauyerNet:
         self.params['b1'] = np.zeros(output_size)
 
     def predict(self, x):
-        W1, W2 =  self.params['W1'], self.params['W2']
-        
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x,W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1,W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+net = TwoLauyerNet(input_size=784, hidden_size=100, output_size=10)
+print(net.params['W1'].shape)
+print(net.params['b1'].shape)
+print(net.params['W2'].shape)
+print(net.params['b2'].shape)
