b777fe94a5e58d8cf2bf6a453a6cfddf44406c2c
power080900 <power080900@gmail.com>
2023-01-30 17:38:16 +0900

    Deep_Learning_Study_230130

--- dataset/util.py
+++ dataset/util.py
@@ -4,11 +4,42 @@ import sys, os
 sys.path.append(os.pardir)
 import pickle
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+# def sigmoid(x):
+#     return 1 / (1 + np.exp(-x))
 
-def relu(n):
-    return np.maximum(0, n)
+# def relu(n):
+#     return np.maximum(0, n)
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        
+        return out
+    
+    def backward(self, dount):
+        dout[self.mask] = 0
+        dx = dout
+        
+        return dx
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+        
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        
+        return out
+    
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        
+        return dx
 
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
